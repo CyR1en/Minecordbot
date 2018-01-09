fb22a60d00382b175386d2e018eb8841a0f26203
Landon Jobe <jobelandon@gmail.com>
2018-01-09 06:04:51 -0600

    Update build.sh
    Add an exit status (0) on completion.
    Add a method to clear the terminal / tty
    Fix case structure. (MineCordBot >> Minecordbot)

--- build.sh
+++ build.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env sh
 
 # Simple script to automatically calls the gradle wrapper
-# Not entirely necessary, but helps users build without touching the CLI
-echo "This script will attempt to build MineCordBot for you. Please make sure you have ran BuildTools, from SpigotMC."
-echo
+# Benefits:
+# * Allows the user to cancel the process (5s buffer)
+# * Captures the terminal window on finish, allowing users to read through the build log.
+# * Clears the terminal after it's completed.
+
+echo "This script will attempt to build Minecordbot for you."
+sleep 1s
 echo "You have 5 seconds to cancel this script with CTRL+C before we start building."
 sleep 5s
 echo
-echo "Building with Gradle"
+echo "Starting to build Minecordbot."
+sleep 1s
 echo #Start the build script
 sh gradlew.sh build
 echo
 echo "All done! If you had any errors, please check the documentation and/or report the issue on the Discord server."
 echo
-read -n 1 -s -p "Press any key to continue"
+read -n 1 -s -p "Press any key to exit and clean up the screen"
+clear
+exit 0
